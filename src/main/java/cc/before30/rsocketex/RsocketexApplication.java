ea964beaa6aeab51461924a02f8d382cd6d38827
Joseph Yang <before30@gmail.com>
2020-08-31 23:55:04 +0900

    Add baseline

---
+++ src/main/java/cc/before30/rsocketex/RsocketexApplication.java
@@ -0,0 +1,13 @@
+package cc.before30.rsocketex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsocketexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RsocketexApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cc/before30/rsocketex/RsocketexApplicationTests.java
@@ -0,0 +1,13 @@
+package cc.before30.rsocketex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RsocketexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
